da54d38facecd07c4ddd7e44c273e7ab7fb1c47b
longmv88 <93542962+longmv88@users.noreply.github.com>
2022-01-09 20:50:21 +0700

    ok

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -8,11 +8,16 @@ import { Component, OnInit } from '@angular/core';
 export class HomeComponent implements OnInit {
 
   public name = 'Longmv';
-  age = 115;
+  public age = 115;
+  public danhmuc = ['STT', 'Ten Trai Cay', 'Gia Thanh', 'Can Nang', 'tinh trang Sale'];
+  public traiCay = ['Tao','Nho', 'Cam'];
+  public traiCay2 = [{ten: 'Tao', gia: 12, canNang: 89, sale: true},{ten: 'Nho', gia: 13, canNang: 82, sale: false},{ten: 'Cam', gia: 14, canNang: 83, sale: true}];
 
   constructor() { }
 
-  ngOnInit(): void {
+ public ngOnInit(): void {
+   console.log('trai cay =', this.traiCay);
+   console.log('trai cay =', this.traiCay2);
   }
 
 }
